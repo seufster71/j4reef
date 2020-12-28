61d650194e33a80a3c6a92fb76ac664150daedea
edwardseufert <edwardseufert@edwards-mbp.home>
2020-12-28 09:46:20 -0500

    test

---
+++ j4reef/gitaddsubmodule.sh
@@ -0,0 +1,7 @@
+ECHO adding submodules
+
+cd ~/git/j4reef/j4reef/src/main/js
+git submodule add https://github.com/seufster71/toasthub-react-core.git core
+
+cd ~/git/j4reef/j4reef/src/main/js
+git submodule add https://github.com/seufster71/toasthub-reactweb-core.git coreView
